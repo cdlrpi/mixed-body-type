e02f3091cca68ffeaae1a277e1d3fe4003ef3576
Jeremy Laflin <laflijj@gmail.com>
2015-03-10 23:28:21 -0400

    added python version of mass matrix notebook to be run remotely

--- large-deformation/gebf/rope-python/mass-matrix-and-internal-forces-v2.py
+++ large-deformation/gebf/rope-python/mass-matrix-and-internal-forces-v2.py
@@ -3,7 +3,7 @@
 
 # #Beam Element - This notebook computes the mass matrix and internal forces and outputs them for later use
 
-# In[1]:
+# In[ ]:
 
 import numpy as np
 import scipy as sp
@@ -18,12 +18,12 @@ from sympy.interactive import printing
 printing.init_printing(use_latex='mathjax')
 
 
-# In[2]:
+# In[ ]:
 
 sym.mpmath.mp.pretty=True
 
 
-# In[3]:
+# In[ ]:
 
 def Skew_sym(v):
     """
@@ -36,7 +36,7 @@ def Skew_sym(v):
     return v_skew
 
 
-# In[4]:
+# In[ ]:
 
 def Rotate_sym(theta):
     """
@@ -50,7 +50,7 @@ def Rotate_sym(theta):
 
 # #### Define symbolic quantites
 
-# In[5]:
+# In[ ]:
 
 # nodal positons, velocities, and accelerations
 """
@@ -68,12 +68,9 @@ u = sym.Matrix(['u_1x','u_1y','u_1z','u_2x','u_2y','u_2z'])
 udot = sym.Matrix(['udot_1x','udot_1y','udot_1z','udot_2x','udot_2y','udot_2z'])
 uddot = sym.Matrix(['uddot_1x','uddot_1y','uddot_1z','uddot_2x','uddot_2y','uddot_2z'])
 
-theta1 = sym.Matrix('theta_1','phi_1','psi_1')
-theta2 = sym.Matrix('theta_2','phi_2','psi_2')
-theta1_dot = sym.Matrix('thetadot_1','phidot_1','psidot_1')
-theta2_dot = sym.Matrix('thetadot_2','phidot_2','psidot_2')
-theta1_ddot = sym.Matrix('thetaddot_1','phiddot_1','psiddot_1')
-theta2_ddot = sym.Matrix('thetaddot_2','phiddot_2','psiddot_2')
+theta = sym.Matrix(['theta_1x','theta_1y','theta_1z','theta_2x','theta_2y','theta_2z'])
+omega = sym.Matrix(['omega_1x','omega_1y','omega_1z','omega_2x','omega_2y','omega_2z'])
+alpha = sym.Matrix(['alpha_1x','alpha_1y','alpha_1z','alpha_2x','alpha_2y','alpha_2z'])
 
 # locating the point in the cross section
 s1 = sym.Matrix([0, r1])
@@ -81,67 +78,76 @@ s2 = sym.Matrix([0, r2])
 s = sym.Matrix.vstack(s1,s2)
 
 # define state variables
-e = sym.Matrix([u[0:3,0], [theta1[3]], u[3:6,0], [theta2[3]]])
-edot = sym.Matrix([udot[0:3,0], [theta1_dot[3]], udot[3:6,0], [theta2_dot[3]]])
-eddot = sym.Matrix([uddot[0:3,0], [theta1_ddot[3]], uddot[3:6,0], [theta2_ddot[3]]])
-display(e)
+e = sym.Matrix([u[3:6,0], theta[0:3,0], u[3:6,0], theta[3:6,0]])
+edot = sym.Matrix([udot[0:3,0], omega[0:3,0], udot[3:6,0], omega[3:6,0]])
+eddot = sym.Matrix([uddot[0:3,0], alpha[0:3,0], uddot[3:6,0], alpha[3:6,0]])
+# e = u
+# edot = udot
+# eddot = uddot
+display(s)
+display([e,edot,eddot])
 
 
 # ### Needed Matrix Quantities
 
-# In[6]:
+# In[ ]:
 
 # angular velocity for 2D case is trivial
-omega1_skew = Skew_sym(theta1_dot[1:4])
-omega2_skew = Skew_sym(theta2_dot[1:4])
-alpha1_skew = Skew_sym(theta1_ddot[1:4])
-alpha2_skew = Skew_sym(theta2_ddot[1:4])
+omega1_skew = Skew_sym(omega[0:3,0])
+omega2_skew = Skew_sym(omega[3:6,0])
+alpha1_skew = Skew_sym(alpha[0:3,0])
+alpha2_skew = Skew_sym(alpha[3:6,0])
 
-R1 = Rotate_sym(theta1[3])
-R2 = Rotate_sym(theta2[3])
+# Rotation for a 2D problem
+R1 = Rotate_sym(theta[2])
+R2 = Rotate_sym(theta[5])
+# display(R1,R2)
 
+# "spatial" rotation matrix
 R = sym.Matrix.vstack(sym.Matrix.hstack(R1,sym.zeros(3)),                       sym.Matrix.hstack(sym.zeros(3),R2))
 
+# "spatial" angular velocity matrix
 Omega_skew = sym.Matrix.vstack(sym.Matrix.hstack(omega1_skew,sym.zeros(3)),                                sym.Matrix.hstack(sym.zeros(3),omega2_skew))
 
+# "spatial" angular acceleration matrix
 Alpha_skew = sym.Matrix.vstack(sym.Matrix.hstack(alpha1_skew,sym.zeros(3)),                                sym.Matrix.hstack(sym.zeros(3),alpha2_skew))
 # display(Omega)
 
 
 # ### Define Kinematics
 
-# In[7]:
+# In[ ]:
 
 # Define velocity of element endpoints (nodes)
-v = udot + R*Omega_skew*s
+v = sym.simplify(udot + R*Omega_skew*s)
 print('v = ')
 display(v)
 
 # Define acceleration of element endpoints (nodes)
-a = uddot + R*Omega_skew*Omega_skew*s + R*Alpha_skew*s
+a = sym.simplify(uddot + R*Omega_skew*Omega_skew*s + R*Alpha_skew*s)
 print('\na = ')
 display(a)
 
 
 # ### Compute the Mass Matrix
 
-# In[21]:
+# In[ ]:
 
 # Define shape function for element with one node at each end
 h = sym.symarray('h', 2)
 
-h[0] = 1/2*(1 - x)
-h[1] = 1/2*(1 + x)
+h[0] = sym.Rational(1,2)*(1 - x)
+h[1] = sym.Rational(1,2)*(1 + x)
 
 # Compute shape function matrix
 H = sym.expand(sym.Matrix([h[0]*sym.eye(3), h[1]*sym.eye(3)])).T
-# print('\nH = ')
-# display(H)
+print('\nH = ')
+display(H.T)
 
 # Define velocity of any point 
 Vp = H*v
-# print('\nV = ')
-# display(V)
+print('\nV = ')
+display(V)
 
 # Define velocity of any point 
 Ap = H*a
@@ -150,46 +156,51 @@ Ap = H*a
 
 # Compute partial velocities of the nodes
 Vr = sym.Matrix([[sym.diff(Vp[j],edot[i]) 
-                         for j in range(len(V))] for i in range(len(edot))]).T
-
+                         for j in range(len(Vp))] for i in range(len(edot))]).T
 # v_r = H
 print('\nVr = ')
-display(Vr)
+display(Vr.T)
+print(Vr.shape)
 
-# Compute mass matrix
-M = sym.Matrix([[sym.expand(sym.integrate(Vr[:,i].dot(Ap)*rho*A,(x,0,l))).coeff(eddot[j]) for i in range(len(eddot))]
-                for j in range(len(eddot))])
-print('\nM = \n')
-display(M)
-pickle.dump( M, open( "mass-matrix.dump", "wb" ) )
+# # Compute mass matrix
+# M = sym.Matrix([[sym.expand(sym.integrate(Vr[:,i].dot(Ap)*rho*A,(x,0,l))).coeff(eddot[j]) for i in range(len(eddot))]
+#                 for j in range(len(eddot))])
+# print('\nM = \n')
+# display(M)
+# pickle.dump( M, open( "mass-matrix.dump", "wb" ) )
+
+
+# In[ ]:
+
+sym.factor(M,A*rho*l)
 
 
 # ### Compute Internal forces 
 
 # #### Compute Longitudinal Strain Energy
 
-# In[9]:
+# In[ ]:
 
 
 
 
 # #### Compute Transverse Strain Energy
 
-# In[9]:
+# In[ ]:
 
 
 
 
 # #### Compute Internal Forces $Q_e = \frac{\partial U}{\partial e}$
 
-# In[9]:
+# In[ ]:
 
 
 
 
 # ### Applied and body force vector
 
-# In[9]:
+# In[ ]:
 
 
 
