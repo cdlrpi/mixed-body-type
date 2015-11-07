8b0ba75e266936e8145daec673feca36b0a082ca
Jeremy Laflin <laflijj@gmail.com>
2015-11-06 19:19:08 -0500

    reorganize

---
+++ mixed-system-test/DCA.py
@@ -0,0 +1,177 @@
+import pdb
+import numpy as np
+import math
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+import MBstructs as MBS
+import MBfuncts  as MBF
+
+def solve(n,i,bodies,joints,BC1,BC2): 
+    """
+    This function uses the DCA to form and solve the equations of motion.
+    """
+    
+    #This if statements is the recursive case of this function.
+    #Inside this if statement the current list of bodies is
+    #assembled into a smaller list and passed back to this function.
+    if len(bodies) !=1: #if there are still more than one bodies in the system
+        j=0
+        odd = 0
+        
+        #check if there are an odd number of bodies and
+        #create a new list called "newbds" containing the correct number of
+        #new bodies
+        newbds=[]
+        # create new joint list using binary tree assembly
+        newjs = joints[::2]
+        js_assem = joints[1::2]
+
+        if (len(bodies)%2)==0:
+            for k in range (0,math.trunc(len(bodies)/2)):
+                newbds.append(MBS.Rigid_Body2D())
+        else:
+            odd=1
+            for k in range (0,math.trunc(len(bodies)/2)+1):
+                newbds.append(MBS.Rigid_Body2D())
+            
+        #Loop through all of the new bodies, assembling the correct two
+        #"old" bodies to create the new.
+        while j < len(newbds):
+            
+            #if there are an odd # of bodies and we're at the last 
+            #body in newbds, add the last body to newbds
+            if j == len(newbds)-1 and odd == 1:
+                newbds[j].z11 = bodies[2*j].z11
+                newbds[j].z12 = bodies[2*j].z12
+                newbds[j].z21 = bodies[2*j].z21
+                newbds[j].z22 = bodies[2*j].z22
+                newbds[j].z13 = bodies[2*j].z13
+                newbds[j].z23 = bodies[2*j].z23
+                
+            #Otherwise, calculate the new zetas for the newly assembled bodies
+            #according to the formulae
+            else:
+
+                #X, Q, and Y  are  intermediate quantities
+                newbds[j].X=np.dot(joints[2*j+1].D.T,np.dot(bodies[2*j+1].z11+bodies[2*j].z22,joints[2*j+1].D))
+                newbds[j].Xinv=np.linalg.inv(newbds[j].X)
+                newbds[j].W=np.dot(joints[2*j+1].D,np.dot(newbds[j].Xinv,joints[2*j+1].D.T))
+                newbds[j].Y=np.dot(newbds[j].W,bodies[2*j].z23-bodies[2*j+1].z13)#ommitted pdot*u
+               
+                #assemble the bodies based on the formulas
+                newbds[j].z11 = bodies[2*j].z11 - np.dot(bodies[2*j].z12, np.dot(newbds[j].W, bodies[2*j].z21))
+                newbds[j].z22 = bodies[2*j+1].z22 - np.dot(bodies[2*j+1].z21, np.dot(newbds[j].W, bodies[2*j+1].z12))
+                
+                newbds[j].z12 = np.dot(bodies[2*j].z12, np.dot(newbds[j].W, bodies[2*j+1].z12))
+                newbds[j].z21 = np.dot(bodies[2*j+1].z21, np.dot(newbds[j].W, bodies[2*j].z21))
+                
+                newbds[j].z13 = bodies[2*j].z13 - np.dot(bodies[2*j].z12,newbds[j].Y)
+                newbds[j].z23 = bodies[2*j+1].z23 + np.dot(bodies[2*j+1].z21,newbds[j].Y)
+            j=j+1
+            
+        # newjs = [MB.Joint('revolute2D') for k in range (len(newbds))]
+        
+        #This is the recursive call.  This will return a list of the form:
+        #[A11,F1c1,A12,F1c2,A21,F2c1,...,An2,Fnc2] where the Axy's and Fxcy's 
+        #correspond to the accellerations and constraint forces in the 
+        #xth body on the yth handle. The function is given the original 
+        #number of bodies, the list of new bodies, the list of new joints,
+        #the boundary conditions,and the state variables at that timestep.
+        #At this point this function will repeat itself in a loop until there is
+        #one body left
+
+        sol=solve(n,i+1,newbds,newjs,BC1,BC2)
+        #Forces and Accelerations at the new joints are found,
+        #so these values can now be found for the old joints.
+        #newsol will contain the new solution
+        newsol=[];
+        newsol.append(sol[0])
+        newsol.append(sol[1])
+        
+        #In this loop I start with the first body and find the force and 
+        #acceleration on its other handle. These values are added to the new
+        #solution list, along with the next values in sol.
+        flag=0
+        for j in range(0,len(newbds)):
+            # Don't enter this if there are an odd number of bodies and it 
+            # is the last time through the loop, otherwise enter.
+            if not(odd==1 and j==len(newbds)-1):
+                # index counter
+                k=len(newsol)-2
+
+
+                # The force in the joint between two assembled bodies
+                # There is some error with the original indexing of the joint
+                # Fix: create a list of joints that were used to assemble the bodies 
+                #      
+                F=-1*(np.dot(js_assem[j].D,np.dot(newbds[j].Xinv,np.dot(js_assem[j].D.T,\
+                        (np.dot(bodies[2*j].z21,newsol[k+1])-np.dot(bodies[2*j+1].z12,sol[4*j+3])+\
+                        bodies[2*j].z23-bodies[2*j+1].z13)))))
+                # F=-1*(np.dot(joints[j+1].D,np.dot(newbds[j].Xinv,np.dot(joints[j+1].D.T,\
+                #         (np.dot(bodies[2*j].z21,newsol[k+1])-np.dot(bodies[2*j+1].z12,sol[4*j+3])+\
+                #         bodies[2*j].z23-bodies[2*j+1].z13)))))
+                # A2 is the acceleration of handle 2 of body k
+                A2=np.dot(bodies[2*j].z21,newsol[k+1])+np.dot(bodies[2*j].z22,F)+bodies[2*j].z23
+                # A1 is the acceleration of handle 1 of body k+1
+                A1=np.dot(bodies[2*j+1].z11,-1*F)+np.dot(bodies[2*j+1].z12,sol[4*j+3])+bodies[2*j+1].z13
+                # Add the newly found values to new solution list,
+                # maintiaining the format described above.
+                newsol.append(A2)
+                newsol.append(F)
+                newsol.append(A1)
+                newsol.append(-1*F)
+                # Add the next two values in sol to the new solution list
+                newsol.append(sol[4*j+2])
+                newsol.append(sol[4*j+3])
+            
+            #if there are an odd number of bodies and this is the last
+            #time through the loop, append the last force and accelleration 
+            #in the old solution to the new one
+            else:
+                newsol.append(sol[4*j+2])
+                newsol.append(sol[4*j+3])
+                flag=1
+            
+            #If this is not the last time through the loop, append the 
+            #next two values in the old solution to the new solution
+            if j!=len(newbds)-1  and flag !=1:   
+                newsol.append(sol[4*j+4])
+                newsol.append(sol[4*j+5])
+        
+        #If this is the 0th level of recursion, delete all the forces 
+        #so only the accellerations are returned
+        if i ==0:
+            for j in range (1,2*n+1):
+                del newsol[j]
+            return newsol
+        
+        #If this is not the 0th level of recursion, return the solution
+        #with all of the forces included
+        else:
+            return newsol
+    
+    #This is the base case of this recursive function.  This is where
+    #the recursion ends and reassembly begins
+    elif BC1==2 and BC2==1:
+
+        #return the forces and accellerations at the end joints
+        #and begin disassembly
+        Q = np.dot(joints[0].P.T,np.dot(np.linalg.inv(bodies[0].z11),joints[0].P))
+        normQ = np.linalg.norm(Q)
+        if normQ == 0:
+            udot = 0
+        elif Q.size == 1:
+            invQ = 1/Q
+            udot = np.dot(invQ,np.dot(joints[0].P.T,np.dot(np.linalg.inv(bodies[0].z11),bodies[0].z13)))
+        else:
+            invQ = np.linalg.inv(Q)
+            udot = np.dot(invQ,np.dot(joints[0].P.T,np.dot(np.linalg.inv(bodies[0].z11),bodies[0].z13)))
+        A1 = np.dot(joints[0].P,udot)
+        Fc1 = np.dot(np.linalg.inv(bodies[0].z11),A1 - bodies[0].z13)
+        Fc2 = np.zeros_like(Fc1)
+        A2 = np.dot(bodies[0].z21,Fc1) + np.dot(bodies[0].z22,Fc2) + bodies[0].z23
+        
+        sol=[A1,Fc1,A2,Fc2]
+
+        return sol  
---
+++ mixed-system-test/MBfuncts.py
@@ -0,0 +1,211 @@
+# Jeremy Laflin
+# Modified version of MultiBodyFuncts.py by Mike Hans
+# In this version
+#   - Rotation matricies are reduced to 2x2 simple rotation about 3rd axis
+import numpy as np
+#This script will hold some functions that will be used in other scripts
+def DCM(theta):
+    C = np.array([[np.cos(theta), -np.sin(theta)],\
+                  [np.sin(theta),  np.cos(theta)]])
+    return C
+
+# Once these work they should be moved to class functions (maybe)
+# Plotting Functions
+def get_topology_2DRigid(q,l):
+    """
+    This fuction determined the location of the endpoints of a rigid pendlum.  
+    """
+
+    theta = np.cumsum(q,1)
+    C = [[np.array([[np.cos(angle),-np.sin(angle)],\
+                    [np.sin(angle), np.cos(angle)]]) 
+      for angle in tstep] for tstep in theta]
+    R = [[np.dot(CBN,np.array([l,0])) for CBN in timestep] \
+                                      for timestep in C]
+    R = [np.cumsum(r,0) for r in R]
+
+    x = [np.hstack((np.array([0]),r[:,0])) for r in R]
+    y = [np.hstack((np.array([0]),r[:,1])) for r in R]
+
+    return x,y
+    
+def get_topology_2DGEBF(q,r0,l,nelements,npoints):
+    """
+    This function determines the positions of all the nodes of the gebf elements of the system. 
+    """
+    
+    # delete rotational degrees of freedom
+    # remaining dof's are theta1,x1,y1,theta2,x2,y2 --> x1,y1,x2,y2 
+    ntsteps, len_q = np.shape(q)
+    
+    displacements = np.delete(q,range(0,len_q,3),1)
+    
+    # compute position of each node
+    position = r0 + displacements
+    position_element = np.array_split(position,nelements,axis=1)
+    
+    # interpolate 
+    s = np.linspace(-1,1,npoints)
+    h1 = 1/2*(1 - s)
+    h2 = 1/2*(1 + s)
+
+    # Compute shape function matrix
+    H = np.array([np.hstack((h1*np.eye(2), h2*np.eye(2))) 
+                  for h1,h2 in zip(h1,h2)]).reshape(2*npoints,4).T
+    
+    # Interpolate the X and Y cooridnates
+    xy = np.hstack(np.array([np.dot(position_element,H) 
+                             for position_element in position_element]))
+    
+    x = np.array_split(xy[:,0::2],ntsteps+1)
+    y = np.array_split(xy[:,1::2],ntsteps+1)
+
+    return x,y
+    
+
+# Energy Functions
+def get_energy_Rigid(bodies,state):
+    """
+    This function determines the kinetic energy of the rigid-bodies of a pendulum given the generalized coordinates of the system.
+    """
+    
+    ntsteps,nbodies = np.shape(state)
+    q = state[:,:nbodies/2]
+    u = state[:,nbodies/2:]
+
+    theta = np.cumsum(q,1)
+    omega = np.cumsum(u,1)
+
+    C = [[np.array([[np.cos(angle),-np.sin(angle)],\
+                    [np.sin(angle), np.cos(angle)]]) for angle in tstep] 
+                                                     for tstep in theta]
+
+    ## Potential Energy Calculation ##
+    # vector location of joints for all bodies
+    Rh2Body = [[np.dot(CBN,np.array([body.l,0])) 
+                                      for (CBN,body) in zip(timestep,bodies)] 
+                                      for timestep in C]
+    Rh2 = [np.cumsum(r,0) for r in Rh2Body]
+
+    # vector location of center of mass for all bodies
+    # from handle two
+    rcmBody = [[np.dot(CBN,np.array([-body.l/2,0])) 
+                                        for (CBN,body) in zip(timestep,bodies)]
+                                        for timestep in C]
+
+    # locate the centers of mass w.r.t. origin
+    rcm = [np.array(Rbody + rbody) for Rbody,rbody in zip(Rh2,rcmBody)]
+
+    # slice out c.m. y position
+    ycm = [y[:,1] for y in rcm]
+
+    # compute heights of centers of mass
+    hcmRel = [body.l/2 for body in bodies]
+    lcm = np.cumsum([body.l for body in bodies])
+    lcm = lcm.tolist()
+    lcm.pop(len(bodies)-1)
+    lcm.insert(0,0)
+    lcm = np.array(lcm)
+    hcm = [lcm+hcmRel+y for y in ycm]
+
+    pe = np.sum([[9.81*body.m*h 
+                for body, h in zip(bodies,timestep)]
+                for timestep in hcm],1)
+
+    ## Kinetic Energy Calculation ##
+    vh2Body = np.cumsum([[qdot*np.array([-R[1],R[0]]) 
+                for qdot,R in zip(timestep,R)] 
+                for timestep,R in zip(omega,Rh2Body)],1)
+
+    vcmBody = [[qdot*np.array([-r[1],r[0]]) 
+                for qdot,r in zip(tstep,r_tstep)] 
+                for tstep,r_tstep in zip(omega,rcmBody)]
+
+    vcm = vcmBody+vh2Body
+
+    keT = np.sum([[1/2*body.m*np.dot(v,v) 
+                   for body,v in zip(bodies,timestep)] 
+                   for timestep in vcm],1)
+
+    keR = np.sum([[1/2*body.I*qdot**2 
+                for body,qdot in zip(bodies,timestep)]
+                for timestep in omega],1)
+    ke = keT + keR
+    te = ke + pe
+    return ke,pe,te
+
+# Functions to determine generalized accelerations from the spatial accelerations
+# DCA returns Spatial Accelerations
+# Therefore, need to extract generalized accelerations
+def get_gen_accel_Rigid(nbodies, joints, accel):
+    """
+    This function determines the generalized acclerations between rigid-bodies 
+    """
+    udot = np.zeros((nbodies))
+    for j in range(nbodies):
+        if j == 0:
+            A1 = accel.pop(0)
+            udot[j] = np.dot(joints[j].P.T,A1)
+        else:
+            A2 = accel.pop(0)
+            A1 = accel.pop(0)
+            udot[j] = np.dot(joints[j].P.T,(A1-A2))
+    
+    #add the velocities to d_dt and return to the integrator
+    return udot 
+
+def get_gen_accel_GEBF(nGEBF, accel):
+    """
+    This function determines the generalized accelerations between GEBF elements
+    """
+    ndofsGEBF = 6
+    state_dot = np.array(accel,dtype=np.double).reshape(nGEBF*ndofsGEBF)
+    return state_dot 
+
+def kinematics_Rigid2D(bodies,q,u):
+    """
+    This function determines the kinematics of a 2D rigid pendulum
+    """
+
+    for body,theta,omega in zip(bodies,np.cumsum(q),np.cumsum(u)):
+        body.omega = omega
+        body.CBN = DCM(theta)
+            
+def kinematics_GEBF2D(bodies,q,u):
+    """
+    Needs fixing !!!!!!!!!!!!!!!!!!!!!!
+    """
+    #  # slice state into 'qs' for each element 
+    #  thetae = q[::3]
+    #  theta = np.cumsum(thetae)
+
+    #  # slice into the two rotational coordinates for each body
+    #  theta1 = theta[0::2]
+    #  theta2 = theta[1::2]
+    #  for body,q0,q3 in zip(bodies, theta1, theta2):
+    #      body.theta1 = q0
+    #      body.theta2 = q3
+    raise Exception('Fix me!!!!')
+
+def myRK4(func, state0, tspan, *args):
+    """
+    Inputs:
+        func - function that returns the derivatives of the state variables
+          x0 - starting conditions
+           t - array of time values 
+   Outputs:
+       w - approximate solution at the mesh-points (time-steps)
+    """
+    dt = np.max(np.diff(tspan))
+    nsteps = len(tspan)
+    state = np.zeros((nsteps-1,len(state0)))
+    state = np.vstack((state0,state))
+        
+    for i in range(len(tspan)-1):
+        k1 = dt*func(state[i,:],        tspan,*args)
+        k2 = dt*func(state[i,:] + k1/2, tspan,*args)
+        k3 = dt*func(state[i,:] + k2/2, tspan,*args)
+        k4 = dt*func(state[i,:] + k3,   tspan,*args)
+        
+        state[i+1,:] = state[i,:] + (1/6*((k1)+(2*k2)+(2*k3)+(k4))).reshape(1,len(state0))
+    return state
